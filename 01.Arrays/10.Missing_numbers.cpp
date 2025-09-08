a48d2b5fab169f6163b11b8fe6bdb094753c46ee
Harsh Srivastava <121292441+HarshSrivastava-alt@users.noreply.github.com>
2025-09-08 08:47:43 +0530

    10.Missing_numbers.cpp

--- 01.Arrays/10.Missing_numbers.cpp
+++ 01.Arrays/10.Missing_numbers.cpp
@@ -28,7 +28,7 @@ using namespace std;
 int main(){
     int arr[]={1,2,3,4,5,7};
     int n = sizeof(arr)/sizeof(arr[0]);
-    int total = (n+1)*(n+2)/2;
+    int total = (n+1)*n/2;
     int sum = 0;
     for(int i=0;i<n;i++){
         sum+=arr[i];
